48856cff31a7daa9a192967ad0ab4f54929f45a9
Marcelele <marceli.baczewski@gmail.com>
2016-09-27 23:37:14 +0200

    Add functioning mail service.

--- src/main/java/com/marcelib/boatal/service/MailService.java
+++ src/main/java/com/marcelib/boatal/service/MailService.java
@@ -16,15 +16,11 @@ import org.thymeleaf.spring4.SpringTemplateEngine;
 
 
 import javax.inject.Inject;
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.util.Locale;
+import java.util.Properties;
 
-/**
- * Service for sending e-mails.
- * <p>
- * We use the @Async annotation to send e-mails asynchronously.
- * </p>
- */
 @Service
 public class MailService {
 
@@ -32,72 +28,101 @@ public class MailService {
 
     private static final String USER = "user";
     private static final String BASE_URL = "baseUrl";
+    private static final boolean IS_MULTIPART = false;
+    private static final boolean IS_HTML = true;
+
+    private final JHipsterProperties jHipsterProperties;
+
+    private final JavaMailSenderImpl javaMailSender;
+
+    private final MessageSource messageSource;
+
+    private final SpringTemplateEngine templateEngine;
 
     @Inject
-    private JHipsterProperties jHipsterProperties;
+    public MailService(SpringTemplateEngine templateEngine, JavaMailSenderImpl javaMailSender,
+                       JHipsterProperties jHipsterProperties, MessageSource messageSource) {
 
-    @Inject
-    private JavaMailSenderImpl javaMailSender;
-
-    @Inject
-    private MessageSource messageSource;
-
-    @Inject
-    private SpringTemplateEngine templateEngine;
-
-    @Async
-    public void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
-        log.debug("Send e-mail[multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
-            isMultipart, isHtml, to, subject, content);
-
-        // Prepare message using a Spring helper
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        try {
-            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, CharEncoding.UTF_8);
-            message.setTo(to);
-            message.setFrom(jHipsterProperties.getMail().getFrom());
-            message.setSubject(subject);
-            message.setText(content, isHtml);
-            javaMailSender.send(mimeMessage);
-            log.debug("Sent e-mail to User '{}'", to);
-        } catch (Exception e) {
-            log.warn("E-mail could not be sent to user '{}', exception is: {}", to, e.getMessage());
-        }
+        this.templateEngine = templateEngine;
+        this.javaMailSender = javaMailSender;
+        this.jHipsterProperties = jHipsterProperties;
+        this.messageSource = messageSource;
     }
 
     @Async
     public void sendActivationEmail(User user, String baseUrl) {
-        log.debug("Sending activation e-mail to '{}'", user.getEmail());
+        log.debug("Sending activation e-mail to '{}'", user.getEmail()); //only when logging level is set to debug
         Locale locale = Locale.forLanguageTag(user.getLangKey());
-        Context context = new Context(locale);
-        context.setVariable(USER, user);
-        context.setVariable(BASE_URL, baseUrl);
-        String content = templateEngine.process("activationEmail", context);
-        String subject = messageSource.getMessage("email.activation.title", null, locale);
-        sendEmail(user.getEmail(), subject, content, false, true);
+        sendEmail(
+            user.getEmail(),
+            messageSource.getMessage("email.reset.title", null, locale),
+            templateEngine.process("activationEmail", generateContext(locale, user, baseUrl)),
+            IS_MULTIPART,
+            IS_HTML
+        );
     }
 
     @Async
     public void sendCreationEmail(User user, String baseUrl) {
-        log.debug("Sending creation e-mail to '{}'", user.getEmail());
+        log.debug("Sending creation e-mail to '{}'", user.getEmail()); //only when logging level is set to debug
         Locale locale = Locale.forLanguageTag(user.getLangKey());
-        Context context = new Context(locale);
-        context.setVariable(USER, user);
-        context.setVariable(BASE_URL, baseUrl);
-        String content = templateEngine.process("creationEmail", context);
-        String subject = messageSource.getMessage("email.activation.title", null, locale);
-        sendEmail(user.getEmail(), subject, content, false, true);
+        sendEmail(
+            user.getEmail(),
+            messageSource.getMessage("email.reset.title", null, locale),
+            templateEngine.process("creationEmail", generateContext(locale, user, baseUrl)),
+            IS_MULTIPART,
+            IS_HTML
+        );
     }
 
     @Async
     public void sendPasswordResetMail(User user, String baseUrl) {
-        log.debug("Sending password reset e-mail to '{}'", user.getEmail());
+        log.debug("Sending password reset e-mail to '{}'", user.getEmail()); //only when logging level is set to debug
         Locale locale = Locale.forLanguageTag(user.getLangKey());
+        sendEmail(
+            user.getEmail(),
+            messageSource.getMessage("email.reset.title", null, locale),
+            templateEngine.process("passwordResetEmail", generateContext(locale, user, baseUrl)),
+            IS_MULTIPART,
+            IS_HTML
+        );
+    }
+
+    private Context generateContext(Locale locale, User user, String baseUrl) {
         Context context = new Context(locale);
         context.setVariable(USER, user);
         context.setVariable(BASE_URL, baseUrl);
-        String content = templateEngine.process("passwordResetEmail", context);
-        String subject = messageSource.getMessage("email.reset.title", null, locale);
-        sendEmail(user.getEmail(), subject, content, false, true);
+        return context;
+    }
+
+    @Async
+    private void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
+        try {
+            javaMailSender.setJavaMailProperties(generateProperties());
+            javaMailSender.send(generateMimeMessage(to, subject, content, isMultipart, isHtml));
+            log.debug("Sent e-mail to User '{}'", to); //only when logging level is set to debug
+        } catch (MessagingException e) {
+            log.warn("E-mail could not be sent to user '{}', exception is: {}", to, e.getMessage());
+        }
+    }
+
+    private MimeMessage generateMimeMessage(String to, String subject, String content,
+                                            boolean isMultipart, boolean isHtml) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, CharEncoding.UTF_8);
+        message.setTo(to);
+        message.setFrom(jHipsterProperties.getMail().getFrom());
+        message.setSubject(subject);
+        message.setText(content, isHtml);
+        return mimeMessage;
+    }
+
+    private Properties generateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.quitwait", "true");
+        return properties;
     }
 }
